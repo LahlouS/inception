f1ee7e20e3bc12d30a4e4d7afbe876c4f14392cd
Sacha LAHLOU <slahlou@e1r8p6.clusters.42paris.fr>
2023-01-03 10:23:58 +0100

    inception config in progress

--- inception/inception/srcs/requirements/wordpress/conf/auto_config.sh
+++ inception/inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,10 +1,12 @@
 #!bin/bash
 sleep 10
+wp core download --allow-root --locale=fr_FR
 wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/html/wordpress'
+					--dbhost=mariadb:3306 \
+					--path='/var/www/html/wordpress'
 
 wp core install --allow-root --url=https://slahlou.42.fr --title="Inception" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}
 wp user create --allow-root ${WORDPRESS_USER_2} ${WORDPRESS_USER_2_EMAIL} --role=author --user_pass=${WORDPRESS_USER_2_MDP}
