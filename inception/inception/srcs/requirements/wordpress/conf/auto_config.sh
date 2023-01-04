c98e76c1488789731c9063a9ee9edbcf6263ef40
Sacha LAHLOU <slahlou@e1r9p6.clusters.42paris.fr>
2023-01-04 16:33:51 +0100

    inception seems ok

--- inception/inception/srcs/requirements/wordpress/conf/auto_config.sh
+++ inception/inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 sleep 10
+cd /var/www/wordpress
 wp core download --allow-root --locale=fr_FR
 wp config create	--allow-root \
 					--dbname=$SQL_DATABASE \
@@ -7,7 +8,6 @@ wp config create	--allow-root \
 					--dbpass=$SQL_PASSWORD \
 					--dbhost=mariadb:3306 \
 					--path='/var/www/wordpress'
-
 wp core install --allow-root --url=https://slahlou.42.fr --title="Inception" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}
 wp user create --allow-root ${WORDPRESS_USER_2} ${WORDPRESS_USER_2_EMAIL} --role=author --user_pass=${WORDPRESS_USER_2_MDP}
 wp cache flush --allow-root
